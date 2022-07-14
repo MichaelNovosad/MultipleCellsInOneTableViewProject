407756c566c3d76458a6772961fc4fce9ee2aeb6
MichaelNovosad <m.novosad@artk.ai>
2022-07-14 12:00:59 +0300

    1. Create table view 2. Create 3 different cells 3. Add multiple cells in one table view

--- MultipleCellsInOneTableViewProject/ViewController.swift
+++ MultipleCellsInOneTableViewProject/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    private let tableView: UITableView = {
+    let table = UITableView()
+       
+        table.register(ImageTableViewCell.nib(), forCellReuseIdentifier: ImageTableViewCell.identifier)
+        table.register(CodedTableViewCell.self, forCellReuseIdentifier: CodedTableViewCell.identifier)
+        table.register(SimpleTableViewCell.self, forCellReuseIdentifier: SimpleTableViewCell.identifier)
+        
+        return table
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            //show image cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.identifier, for: indexPath) as! ImageTableViewCell
+            cell.configure(with: "image1")
+            return cell
+        }
+        if indexPath.row == 1 {
+            //show image cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: CodedTableViewCell.identifier, for: indexPath) as! CodedTableViewCell
+            cell.configure()
+            return cell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: SimpleTableViewCell.identifier, for: indexPath)
+        cell.textLabel?.text = "Hello world"
+        return cell
+    }
+    
 }
 
